0244260eb303ea246fcd9820da113a8f46f4364f
s-a-malik <shreshth_malik@yahoo.co.uk>
2022-10-08 11:16:20 -0500

    verified

--- back-end/sources/etherscan.py
+++ back-end/sources/etherscan.py
@@ -1,7 +1,7 @@
 """Get data from etherscan.io
 """
 
-import requests
+from utils.api_requests import get_request
 # import pandas as pd
 # from pandas import json_normalize
 import os
@@ -10,43 +10,40 @@ from dotenv import load_dotenv
 load_dotenv()   # load .env file
 ETHERSCAN_KEY = os.getenv("ETHERSCAN_KEY")
 
-#Simple query function
-def query(url):
-    """
-    Returns api result
-    @params:
-        url       - Required  : api url
-    """  
-    request = requests.get(url)
-    if request.status_code == 200:
-        return request.json()
+
+def _make_base_etherscan_url(chain="mainnet"):
+    if chain == "mainnet":
+        base_url = "https://api.etherscan.io/api"
+    elif chain == "ropsten":
+        base_url = "https://api-ropsten.etherscan.io/api"
+    elif chain == "goerli":
+        base_url = "https://api-goerli.etherscan.io/api"
     else:
-        return ('Query failed and return code is {}.      {}'.format(request.status_code, query))
+        raise ValueError(f"Chain {chain} not supported")
+    return base_url
 
-#Query etherscan
-def get_transactions(address, tx_view = 'ERC20'):
-    if tx_view == 'ERC20':
-        view = 'tokentx'
-    elif tx_view == 'normal':
-        view = 'txlist'
-    
-    
-    url = f"""https://api.etherscan.io/api?module=account&action={view}&address={address}&page=1&offset=0&startblock=0&endblock=99999999999&sort=des&apikey={ETHERSCAN_KEY}"""
-    request = query(url)
-    result = json_normalize(request['result'])
-    result['date'] = pd.to_datetime(result.timeStamp, unit='s')
 
-    if tx_view == 'ERC20':
-        result['value'] = result.loc[:,'value'].astype(float) / (10**(result.loc[:,'tokenDecimal'].astype(float)))
-        result = result.loc[:,['blockNumber', 'timeStamp', 'date', 'hash', 'nonce', 'blockHash', 'from',
-        'contractAddress', 'to', 'value', 'tokenName', 'tokenSymbol',
-        'tokenDecimal', 'transactionIndex', 'gas', 'gasPrice', 'gasUsed',
-        'cumulativeGasUsed', 'input', 'confirmations']]
-    return result
+def is_verified(address, chain="mainnet"):
+    # construct the request
+    base_url = _make_base_etherscan_url(chain)
+    params = {
+        "module": "contract",
+        "action": "getabi",
+        "address": address,
+        "apikey": ETHERSCAN_KEY
+    }
+    response = get_request(base_url, params=params)
+    response = response.json()
+    verified = True if response["status"] == "1" else False
 
-#Example
-# address = '<>address'
-# res = get_transactions(address, tx_view='normal')
+    return verified
+
+
+def is_audited(address, chain="mainnet"):
+    raise(NotImplementedError)
+
 
 if __name__ == "__main__":
     print(ETHERSCAN_KEY)
+    print(is_verified("0x984e7B3f332a2a6Fc1EB73B5B8F8E95D24ee2097"))
+    print(is_verified("0x984e7B3f332a2a6Fc1EB73B5B8F8E95D24ee2097", chain="goerl"))
