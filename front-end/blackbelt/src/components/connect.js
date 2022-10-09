3eb6b56c589c0172a0734c8b62eb447ce3582540
s-a-malik <shreshth_malik@yahoo.co.uk>
2022-10-09 03:43:27 -0500

    connect to metamask

--- front-end/blackbelt/src/components/connect.js
+++ front-end/blackbelt/src/components/connect.js
@@ -1,10 +1,12 @@
-import {Button, Box, Text, Center } from "@chakra-ui/react"
+import {Button, Box, Text, Center, Link} from "@chakra-ui/react"
 
 export default function ConnectWalletButton() {
     return (<Center>
-        <Button colorScheme='purple' type='submit' className="connectButton">
+         <a href="http://10.60.3.32:3000/">
+         <Button colorScheme='purple' type='submit' className="connectButton">
             Add to MetaMask Snap
         </Button>
+        </a>
         </Center>
     )
 }
